7ee3f82bc9dbd10d236b8d4aa6611981ce1d0381
Jörg Matysiak <joemat@elinja.de>
2020-05-07 11:41:45 +0200

    * user mockito to mock sms and email service * pass jacoco report path to sonarqube

--- src/test/java/de/wirvsvirus/testresult/integrationtests/IntegrationTestBase.java
+++ src/test/java/de/wirvsvirus/testresult/integrationtests/IntegrationTestBase.java
@@ -3,19 +3,18 @@ package de.wirvsvirus.testresult.integrationtests;
 import de.wirvsvirus.testresult.database.TestResult;
 import de.wirvsvirus.testresult.service.EmailService;
 import de.wirvsvirus.testresult.service.SmsServiceProvider;
+import io.quarkus.test.junit.mockito.InjectMock;
 import org.junit.jupiter.api.BeforeEach;
-
-import javax.inject.Inject;
 
 public class IntegrationTestBase {
 
     public static final String POST_USER = "user";
     public static final String POST_USER_PASS = "userpass";
 
-    @Inject
+    @InjectMock
     EmailService emailServiceMock;
 
-    @Inject
+    @InjectMock
     SmsServiceProvider smsServiceProviderMock;
 
     @BeforeEach
